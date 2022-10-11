de453e568911079d54b4e87f3a664b3b20a45cc5
QuentinHelion <helion.quentin@gmail.com>
2022-10-11 23:05:12 +0200

    add "kéké mode"

--- generator.py
+++ generator.py
@@ -1,7 +1,54 @@
 import string
 import secrets
 
-def generate(length):
-    alphabet = string.ascii_letters + string.digits + string.punctuation
-    password = ''.join(secrets.choice(alphabet) for i in range(length))
-    return password
+# Constantes
+MIN_LENGTH = 12
+DEFAULT_COMPLEXITY = 3
+DEFAULT_NUMBER_PASSWORD_CREATION = 5
+
+class Generator:
+
+    def __init__(self):
+        # Complexity of password:
+        # 1 => letter only
+        # 2 => letter + number
+        # 3 => letter + number + punctuation
+        # Default: max complexity (3)
+        self.complexity = DEFAULT_COMPLEXITY
+
+        # Length of Password
+        # Default: minimum
+        self.length = MIN_LENGTH
+
+        # Number of password generated per try
+        # Default: 5
+        self.nbrOfPwd = DEFAULT_NUMBER_PASSWORD_CREATION
+
+
+    # All function permite to change parameter of generator
+    def setComplexity(self, data):
+        self.complexity = data
+
+    def setLength(self, data):
+        self.length = data
+
+    def setNbr(self, data):
+        self.nbrOfPwd = data
+
+    # Creation of password
+    def create(self):
+        # Complexity of password:
+        # 1 => letter only
+        # 2 => letter + number
+        # 3 => letter + number + punctuation
+        if self.complexity == 1:
+            alphabet = string.ascii_letters
+        elif self.complexity == 2:
+            alphabet = string.ascii_letters + string.digits
+        else:
+            alphabet = string.ascii_letters + string.digits + string.punctuation
+
+        # Creating x password
+        for i in range(self.nbrOfPwd):
+            pwd = ''.join(secrets.choice(alphabet) for i in range(self.length))
+            print(pwd)
--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 from generator import *
 from menu import *
 
-choice = 1
+# Password generator object
+generator = Generator()
 
+choice = 1
 while(choice):
-    choice = menu()
+
+    # Print main menu
+    choice = menu(generator.complexity, generator.length)
+
+    # First choice permite to generate and print the password
     if choice == 1:
-        length = getLength();
-        pwd = generate(length)
-        print(pwd)
-    elif choice == "0":
-        exit()
+        generator.create()
+
+    # Add setting feature
+    # permite to change length or complexity of password
+    elif choice == 2:
+        sChoice = 1
+        while(sChoice):
+            sChoice = setting(generator.complexity, generator.length, generator.nbrOfPwd)
+            if(sChoice) == 1:
+                generator.setLength(changeLength())
+            elif(sChoice) == 2:
+                generator.setComplexity(changeComplexity())
+            elif(sChoice) == 3:
+                generator.setNbr(changeNbrOfPwd())
